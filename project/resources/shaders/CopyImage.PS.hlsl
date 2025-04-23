a6d50c07b3911d00f897eae711dd1f130ee5bdb9
ryusei9 <k023g0066@g.neec.ac.jp>
2025-04-23 15:15:28 +0900

    hlslの追加

---
+++ project/resources/shaders/CopyImage.PS.hlsl
@@ -0,0 +1,15 @@
+#include "CopyImage.hlsli"
+
+Texture2D<float32_t4> gTexture : register(t0);
+SamplerState gSampler : register(s0);
+
+struct PixelShaderOutput
+{
+	float32_t4 color : SV_TARGET;
+};
+PixelShaderOutput main(VertexShaderOutput input)
+{
+	PixelShaderOutput output;
+	output.color = gTexture.Sample(gSampler, input.texcoord);
+	return output;
+}
